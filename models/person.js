585d2da2f9d0ffdd368fc84ac26570328c92a6a9
Sami K <sami.karna@gmail.com>
2018-07-21 19:56:40 +0300

    Separate production and dev databases

--- models/person.js
+++ models/person.js
@@ -1,19 +1,21 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
-
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url, options = { useNewUrlParser: true })
 
 const personSchema = new Schema({ name: String, number: String });
 
-personSchema.statics.format = function(person) {
+personSchema.statics.format = function (person) {
     return {
         name: person.name,
         number: person.number,
         id: person._id
-      }
-  };
+    }
+};
 
 const Person = mongoose.model('Person', personSchema)
 
